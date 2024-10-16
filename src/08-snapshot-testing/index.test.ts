dedb148ae29ce74cf6c2d18406f14d75d4f3615a
vigo44 <vvigo@rambler.ru>
2024-10-17 01:13:50 +0400

    feat: add snapchot-testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,24 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList([1, 3, 5, 2, 4])).toStrictEqual({
+      value: 1,
+      next: {
+        value: 3,
+        next: {
+          value: 5,
+          next: {
+            value: 2,
+            next: { value: 4, next: { value: null, next: null } },
+          },
+        },
+      },
+    });
   });
 
-  // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList([1, 3, 5, 2, 4])).toMatchSnapshot();
   });
 });
